241bf2115073730bcca6bc584e7ab74ad49a9687
Nicholas simone <simonen01@210206-2026.local>
2022-09-03 12:56:09 +1000

    Added upgrade teen-worker

---
+++ upgrades/teen-worker/teen-worker.go
@@ -0,0 +1,24 @@
+package teenworker
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+
+
+func New() {
+
+}
+
+func () Setup() {
+
+}
+
+func () Draw() {
+
+}
+
+func () Update() {
+
+}
+
